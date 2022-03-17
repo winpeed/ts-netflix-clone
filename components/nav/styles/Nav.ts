251814720957c93527b85406b874b7fa1eaaa7fe
Praise Obende <davidobende@gmail.com>
2022-03-17 15:15:10 +0100

    Add UI modifications ti index page

--- components/nav/styles/Nav.ts
+++ components/nav/styles/Nav.ts
@@ -4,12 +4,16 @@ export const Container = styled.nav`
 display: flex;
 justify-content: space-between;
 align-items: center;
-padding: 1em 2em;
+padding: 1em 1.5em;
+
+@media(min-width: 760px){
+    padding: 1em 3em;
+}
 `
 
 export const Button = styled.button`
 background: var(--color-red);
-padding: 0.4em 0.6em;
+padding: 0.5em 0.7em;
 font-size: 0.9rem;
 color:var(--color-white);
 border: none;
@@ -18,6 +22,10 @@ border-radius: 5px;
 a{
     color: var(--color-white);
     text-decoration: none;
+}
+
+@media(min-width: 760px){
+    padding: 0.7em 1.1em;
 }
 `
 
